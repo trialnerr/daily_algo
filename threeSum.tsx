4d00db78ae70cd0dcac1c357b8612fac81f8d923
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-16 18:58:54 -0500

    incomplete

---
+++ threeSum.tsx
@@ -0,0 +1,19 @@
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// loop through the array of nums and for each number find the two elements that add up to -num 
+//define a function twoSum that takes in a 
+function threeSum(nums: number[]): number[][]  {
+  const triplets: number[][] = [];
+  for (let i = 0; i < nums.length; i++) {
+     
+
+  }
+  return triplets;
+}
+
+
+function twoSum(nums, target): number[] {
+  
+}
+
+//example [-1, 1, 2, 3, 4, -2, 3, 0]
